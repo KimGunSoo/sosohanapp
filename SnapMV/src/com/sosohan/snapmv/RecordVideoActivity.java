4b8a2f3ba28a2e364b5a6480119a4f5e3baab4ff
lightreal <Leelightreal@gmail.com>
2013-07-08 22:06:10 +0900

    contiguous playing two mp4 files

--- SnapMV/src/com/sosohan/snapmv/RecordVideoActivity.java
+++ SnapMV/src/com/sosohan/snapmv/RecordVideoActivity.java
@@ -1,5 +1,8 @@
 package com.sosohan.snapmv;
 
+import java.util.ArrayList;
+
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -9,11 +12,17 @@ import android.database.Cursor;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class RecordVideoActivity extends Activity {
 	
 	public static final int REC_MOV = 1;
+	private static final String MOVE_URL = "/mnt/sdcard/mp4_sample_first.mp4";
+	private ArrayList<String> array = new ArrayList<String>();
+	private int count;
 	
 	private Button btnVideoRecord;
 	private View.OnClickListener btnClickListener;
@@ -21,6 +30,7 @@ public class RecordVideoActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_record_video);
 
 		btnVideoRecord = (Button) findViewById(R.id.record_btn);
@@ -38,6 +48,36 @@ public class RecordVideoActivity extends Activity {
 		};
 		
 		btnVideoRecord.setOnClickListener(btnClickListener);
+		
+		array.add("/mnt/sdcard/mp4_sample_first.mp4");
+        array.add("/mnt/sdcard/mp4_sample_second.mp4");
+        count = 0;
+        final VideoView videoView = (VideoView)findViewById(R.id.videoView1);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        
+        Uri video = Uri.parse(MOVE_URL);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(video);
+        videoView.requestFocus();
+        
+        MediaPlayer.OnCompletionListener mComplete = new MediaPlayer.OnCompletionListener() {			
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				count++;
+				if(array.size() > count) {
+					Uri video1 = Uri.parse(array.get(count).toString());					
+					videoView.setVideoURI(video1);
+					videoView.start();
+				} else {
+					Uri video2 = Uri.parse(array.get(0).toString());
+					videoView.setVideoURI(video2);
+					count = 0;
+				}
+			}
+		};
+		videoView.setOnCompletionListener(mComplete);
+		videoView.start();
 	}
 	
 	public void onActivityResult(int requestCode, int resultCode, Intent intent)
@@ -51,12 +91,12 @@ public class RecordVideoActivity extends Activity {
 	            String path = getPath(uri);
 	            String name = getName(uri);
 	            String uriId = getUriId(uri);
-	            Log.e("JWJWJW", "실제경로 : " + path + "\n파일명 : " + name + "\nuri : " + uri.toString() + "\nuri id : " + uriId);
+	            Log.e("JWJWJW", "�ㅼ젣寃쎈줈 : " + path + "\n�뚯씪紐�: " + name + "\nuri : " + uri.toString() + "\nuri id : " + uriId);
 	        }
 	    }	   
 	}
 	
-	// 실제 경로 찾기
+	// �ㅼ젣 寃쎈줈 李얘린
 	private String getPath(Uri uri)
 	{
 	    String[] projection = { MediaStore.Images.Media.DATA };
@@ -66,7 +106,7 @@ public class RecordVideoActivity extends Activity {
 	    return cursor.getString(column_index);
 	}
 	 
-	// 파일명 찾기
+	// �뚯씪紐�李얘린
 	private String getName(Uri uri)
 	{
 	    String[] projection = { MediaStore.Images.ImageColumns.DISPLAY_NAME };
@@ -77,7 +117,7 @@ public class RecordVideoActivity extends Activity {
 	    return cursor.getString(column_index);
 	}
 	 
-	// uri 아이디 찾기
+	// uri �꾩씠��李얘린
 	private String getUriId(Uri uri)
 	{
 	    String[] projection = { MediaStore.Images.ImageColumns._ID };
