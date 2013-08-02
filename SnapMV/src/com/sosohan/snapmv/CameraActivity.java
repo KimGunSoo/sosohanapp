824eb75eb3b5a1bfd4126c36c8ea3d1a9862484f
jw <junwooo.kim@samsung.com>
2013-08-02 14:41:36 +0900

    camera rotation enable... but, not good feature.

--- SnapMV/src/com/sosohan/snapmv/CameraActivity.java
+++ SnapMV/src/com/sosohan/snapmv/CameraActivity.java
@@ -11,10 +11,13 @@ import android.media.MediaRecorder.OnInfoListener;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -23,6 +26,7 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ImageView;
 import android.widget.ToggleButton;
+
 
 public class CameraActivity extends Activity implements SurfaceHolder.Callback, OnInfoListener{
 	String cam_tag = "CameraActivity";
@@ -40,16 +44,15 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_camera);
+		//setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		logoBtn = (ImageView)  findViewById(R.id.logo_btn);
 		camSurfaceView = (SurfaceView) findViewById(R.id.camSurfaceView);
 		
 		logoBtn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				recording = true;
 				start();
-				//camSurfaceView.setClickable(false);
 				logoBtn.setImageResource(R.drawable.ic_snap_cam_rec);
 				if(idx >= 8)
 					idx = 1;
@@ -63,6 +66,8 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);//this is a deprecated method, is not required after 3.0 
         		
 		camProfile = CamcorderProfile.get(CamcorderProfile.QUALITY_TIME_LAPSE_480P);
+		//if s3
+		//camProfile.videoFrameWidth = 720;
 		Log.v(cam_tag,"w" + camProfile.videoFrameWidth + "/h"+ camProfile.videoFrameHeight); 
 	}
 	@Override
@@ -134,28 +139,24 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 		}	
 		return result;		
 	}
+	
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		Log.i(cam_tag, "surfaceCreated");
-		DisplayMetrics displayMetrics = new DisplayMetrics(); 
-		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-		
-		int height = displayMetrics.heightPixels;
-		holder.setFixedSize((camProfile.videoFrameWidth*displayMetrics.heightPixels)/camProfile.videoFrameHeight, height);
-		//Log.d(cam_tag+"vd", "size="+(camProfile.videoFrameWidth*height)/camProfile.videoFrameHeight+", "+height);
+		resizeCamSurfaceView();
 		camera = Camera.open( getCameraInfo(Camera.CameraInfo.CAMERA_FACING_FRONT) );	
 	}	
 	private boolean previewRunning;
 	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+	public void surfaceChanged(SurfaceHolder holder, int pixelFormat, int width,
 			int height) {
-		Log.i(cam_tag, format+"surfaceChanged"+width+"*"+height);		
-		if (previewRunning){
+		Log.i(cam_tag, "::surfaceChanged::"+width+"*"+height);	
+				
+		if (previewRunning) {
 			camera.stopPreview();
 		}
 		Camera.Parameters p = camera.getParameters();
-		if(p != null)
-		{
+		if(p != null) {
 			List<Camera.Size> sizeList = p.getSupportedPreviewSizes();
 			for(Size size : sizeList)
 				Log.d(cam_tag+"prv", "size="+size.width+", "+size.height);
@@ -163,10 +164,12 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 			for(Size size : sizeList)
 				Log.d(cam_tag+"vd", "size="+size.width+", "+size.height);
 		}
-		p.setPreviewSize(camProfile.videoFrameWidth, camProfile.videoFrameHeight);
+		
+//		p.setPreviewSize(camProfile.videoFrameWidth, camProfile.videoFrameHeight);
+//		p.setPreviewSize(1280, 720);
 //		p.setPreviewFormat(PixelFormat.JPEG);
 		camera.setParameters(p);
-
+		
 		try {
 			camera.setPreviewDisplay(holder);
 			camera.startPreview();
@@ -177,7 +180,37 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 			e.printStackTrace();
 		}
 	}
-	
+	private void resizeCamSurfaceView()
+	{
+		DisplayMetrics displayMetrics = new DisplayMetrics(); 
+		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+		
+		int DisplayWidth = displayMetrics.widthPixels;
+		int DisplayHeight = displayMetrics.heightPixels;
+		
+		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){			
+			holder.setFixedSize(DisplayWidth, (DisplayWidth*camProfile.videoFrameWidth/camProfile.videoFrameHeight));			
+		}
+		else{					
+			holder.setFixedSize((camProfile.videoFrameWidth*DisplayHeight)/camProfile.videoFrameHeight, DisplayHeight);
+		}
+//		int rotation = getWindowManager().getDefaultDisplay()
+//                .getRotation();
+//		 switch (rotation) {
+//         case Surface.ROTATION_0:
+//        	 Log.e("JWJWJW", "rot 0");
+//             camera.setDisplayOrientation(90);
+//             break;
+//         case Surface.ROTATION_90:
+//        	 Log.e("JWJWJW", "rot 90");
+//        	 camera.setDisplayOrientation(0);
+//             break;
+//         case Surface.ROTATION_180:
+//        	 Log.e("JWJWJW", "rot 180");
+//        	 camera.setDisplayOrientation(180);
+//             break;         
+//         }
+	}
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		Log.i(cam_tag, "surfaceDestroyed");	
@@ -193,11 +226,9 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 			mr.stop();			
 			recorder.release();
 			recording = false;
-			//camSurfaceView.setClickable(true);
 			logoBtn.setImageResource(R.drawable.ic_snap_cam);
 		}
-	}
-	
+	}	
 	boolean recording = false;
 //	@Override
 //	public boolean onTouchEvent(MotionEvent event) {
@@ -220,5 +251,17 @@ public class CameraActivity extends Activity implements SurfaceHolder.Callback, 
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.camera, menu);
 		return true;
+	}
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+			resizeCamSurfaceView();	
+			camera.setDisplayOrientation(0);
+		}else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+			resizeCamSurfaceView();	
+			camera.setDisplayOrientation(90);
+		}
+		
+		super.onConfigurationChanged(newConfig);		
 	}
 }
--- SnapMV/src/com/sosohan/snapmv/MainActivity.java
+++ SnapMV/src/com/sosohan/snapmv/MainActivity.java
@@ -42,8 +42,7 @@ public class MainActivity extends Activity {
 					Intent intent = new Intent(MainActivity.this,
 							SettingsActivity.class);
 					startActivity(intent);
-				}
-				
+				}				
 			}
 		};
 
